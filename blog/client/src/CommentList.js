1d68d67d768357b553ef66d0e520e52edbb37d3d
Tobias Zillmer <nqxdev@gmail.com>
2021-01-28 20:44:11 +0100

    added client
    .

---
+++ blog/client/src/CommentList.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+//import axios from 'axios';
+
+export default ({comments}) => {
+    //const [comments, setComments] = useState([]);
+
+    // const fetchData = async () => {
+    //     const res = await axios.get(`http://localhost:4001/${postId}/comments`);
+
+    //     setComments(res.data);
+    // }
+
+    // useEffect(() => {
+    //     fetchData();
+    // }, []);
+
+    const renderedComments = comments.map(comment => {
+        let content;
+
+        if(comment.status === 'approved') {
+            content = comment.content;
+        }
+        if(comment.status === 'pending') { 
+            content = 'This comment is awaiting moderation';
+        }
+        if(comment.status == 'rejected') {
+            content = 'This comment has been rejected';
+        }
+
+        return <li key={comments.id} >{content}</li>
+    });
+
+    return <ul>{renderedComments}</ul>
+}
