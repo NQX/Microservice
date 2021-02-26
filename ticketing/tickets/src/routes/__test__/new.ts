d0baf0fa88b9cde2ac0b66310f92d23a41cd95bb
Tobias Zillmer <nqxdev@gmail.com>
2021-02-26 22:15:19 +0100

    basic ticket service

---
+++ ticketing/tickets/src/routes/__test__/new.ts
@@ -0,0 +1,10 @@
+import express, {request, response} from 'express';
+
+const router = express.Router();
+
+router.post('/api/tickets', (req: Request, res: Response) => {
+    res.sendStatus(200);
+});
+
+
+export { router as createTicketRouter }
