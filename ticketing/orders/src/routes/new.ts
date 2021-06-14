d785ee44f9251489991b940c1b5efbe6ebdeb8ae
NQX <nqxdev@gmail.com>
2021-06-14 23:05:40 +0200

    wip 14.6.21

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@nqx1/common';
+import { requireAuth, validateRequest, NotFoundError, OrderStatus } from '@nqx1/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -13,6 +15,33 @@ router.get('/api/orders', requireAuth, [
         .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
         .withMessage('TicketId myst be provided')
 ], validateRequest, async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in DB
+    const ticket = await Ticket.findById(ticketId);
+    if(!ticket) {
+        throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+    const order = await Order.findOne( {
+        ticket: ticket,
+        status: {
+            $in: [
+                OrderStatus.Created,
+                OrderStatus.AwaitingPayment,
+                OrderStatus.Complete
+            ]
+        }
+    });
+
+
+    // Calculate an expiration date for this order
+
+    // Build the order and save it to the DB
+
+    // Publish an event saying that an order was created
+
     res.send({});
 });
 
