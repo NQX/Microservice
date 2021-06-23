c1f150d58205fca5a164a2594bea5082040d23d5
NQX <nqxdev@gmail.com>
2021-06-23 18:40:19 +0200

    done with order service

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest, NotFoundError, OrderStatus } from '@nqx1/common';
+import { requireAuth, validateRequest, NotFoundError, OrderStatus, BadRequestError } from '@nqx1/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.get('/api/orders', requireAuth, [
     body('ticketId')
@@ -23,26 +24,33 @@ router.get('/api/orders', requireAuth, [
         throw new NotFoundError();
     }
 
+
     // Make sure that this ticket is not already reserved
-    const order = await Order.findOne( {
-        ticket: ticket,
-        status: {
-            $in: [
-                OrderStatus.Created,
-                OrderStatus.AwaitingPayment,
-                OrderStatus.Complete
-            ]
-        }
-    });
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+        throw new BadRequestError('Ticket is already reserved');
+    }
 
 
     // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
     // Build the order and save it to the DB
+    const order = Order.build({
+        userId: req.currentUser!.id,
+        status: OrderStatus.Created,
+        expiresAt: expiration,
+        ticket
+    });
+    await order.save();
+
 
     // Publish an event saying that an order was created
+    
 
-    res.send({});
+
+    res.status(201).send(order);
 });
 
 
--- ticketing/orders/src/routes/show.ts
+++ ticketing/orders/src/routes/show.ts
@@ -1,10 +1,23 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, NotFoundError, NotAuthorizedError } from '@nqx1/common';
+import { Order } from '../models/order';
+
 
 const router = express.Router();
 
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-    res.send({});
+router.get('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+
+    if (!order) {
+        throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+
+    res.send(order);
 });
 
 
